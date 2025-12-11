45807f02a1ca1d3f15735c0dcf6e09c53e0679c8
OsvaldoRF098 <osvaldoramirezflores098@gmail.com>
2025-12-11 03:39:42 -0600

    Fix assets mixed content on HTTPS

--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,24 @@ Route::get('/', function () {
 });
 
 // =================== RUTAS DE AUTENTICACIÓN (Jetstream) ===================
-// TEMPORAL: quitar auth para pruebas. REVERTIR después.
-Route::group([], function () {
+Route::middleware(['auth', 'verified'])->group(function () {
 
+    // Cambia el dashboard para que vaya directo a la lista de países
     Route::get('/dashboard', function () {
         return redirect()->route('countries.index');
     })->name('dashboard');
 
+    // Descargar PDF
     Route::get('/countries/export/pdf', [CountryController::class, 'pdf'])
         ->name('countries.pdf');
 
+    // CRUD de países
     Route::resource('countries', CountryController::class);
 
     Route::get('/countries/create', \App\Livewire\CountryCreate::class)->name('countries.create');
     Route::get('/countries/{country}/edit', \App\Livewire\CountryEdit::class)->name('countries.edit');
 
 });
-
 
 // Las rutas de login, registro, forgot-password, etc. las genera Jetstream automáticamente
 // NO necesitas incluir ningún jetstream.php
